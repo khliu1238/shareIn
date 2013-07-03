529129714123e8b4d5f782b30a6eb0029b0d3c45
Howard Liu <genhau@gmail.com>
2013-07-03 12:10:39 +0800

    tiny changes

--- shareIn/urls.py
+++ shareIn/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+    
     # Examples:
     # url(r'^$', 'shareIn.views.home', name='home'),
     # url(r'^shareIn/', include('shareIn.foo.urls')),
